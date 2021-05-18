ec0655506056927e4820cd89b2fc439157785e72
skdud4659 <skdud4659@gmail.com>
2021-05-18 19:58:11 +0900

    community 글쓰기 post

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, jsonify, redirect, session, url_for, flash
 from datetime import timedelta
 from pymongo import MongoClient
+
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.dbhanghae2
 
-#메인
+
+# 메인
 @app.route('/')
 def home():
     return render_template('home.html')
 
-#로그인
+
+# 로그인
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -28,14 +31,18 @@ def login():
             return redirect('/')
         return redirect('/login')
 
-#로그아웃
+
+# 로그아웃
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect('/')
 
-#회원가입
+
+# 회원가입
 app.config["SECRET_KEY"] = "hanghaeeeeee"
+
+
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "GET":
@@ -59,55 +66,60 @@ def register():
             flash('회원가입 완료, 로그인을 해주세요.')
             return redirect('/login')
 
-#about us
+
+# about us
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
 
-#크루 모집
+
+# 크루 모집
 @app.route('/makecrew')
 def makecrew():
     return render_template('makecrew.html')
 
-#크루 참가
+
+# 크루 참가
 @app.route('/joincrew')
 def joincrew():
     return render_template('makecrew.html')
 
-#shop
+
+# shop
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
 
-#커뮤니티
+# 커뮤니티
 @app.route('/community')
 def community():
     return render_template('/community/community.html')
 
-#커뮤니티 - 러닝
-@app.route('/community&running')
-def running():
-    return render_template('/community/running.html')
-
-#커뮤니티 - 클라이밍
-@app.route('/community&climbing')
-def climbing():
-    return render_template('/community/climbing.html')
-
-#커뮤니티 - 라이딩
-@app.route('/community&riding')
-def riding():
-    return render_template('/community/riding.html')
-
-#커뮤니티 - 등산
-@app.route('/community&hiking')
-def hiking():
-    return render_template('/community/hiking.html')
-
-#커뮤니티 - 글쓰기
+# 커뮤니티 - 글쓰기
 @app.route('/community&write')
 def write():
     return render_template('/community/write.html')
 
+@app.route('/community&write&get', methods=["GET", "POST"])
+def posting():
+    if request.method == "POST":
+        tag_receive = request.form['tag_give']
+        title_receive = request.form['title_give']
+        cmt_receive = request.form['cmt_give']
+
+        doc = {
+            'tag': tag_receive,
+            'title': title_receive,
+            'cmt': cmt_receive,
+        }
+        db.post.insert_one(doc)
+
+        return jsonify({'msg': '작성이 완료되었습니다.'})
+
+    elif request.method =='GET':
+        posts = list(db.post.find({}, {'_id': False}))
+        return jsonify({'all_posts': posts})
+
+
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
