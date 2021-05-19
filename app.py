ce8ef8a434260218a85c73c72d5c556d2bb66f68
skdud4659 <skdud4659@gmail.com>
2021-05-19 16:25:01 +0900

    전체 메뉴 및 about 페이지 수정
    - 전체 메뉴바 활성화 class 적용
    - about us 페이지 수정
    - 이메일 보낼 수 있도록 페이지 생성

--- app.py
+++ app.py
@@ -143,6 +143,11 @@ def posting():
         posts = list(db.post.find({}, {'_id': False}))
         return jsonify({'all_posts': posts})
 
+#이메일보내기
+@app.route('/getintouch')
+def getintouch():
+    return render_template('/getintouch.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
